8d06cac571b02165b83d2e4253ec21da152457ff
Ruben Rodriguez <jrubenrm@hotmail.com>
2025-10-27 15:37:32 -0600

    API URL updated

--- patient-frontend/src/api/axiosConfig.js
+++ patient-frontend/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/v1';
+const API_URL = 'http://patientlogapp-env.eba-mvdxr92m.us-east-2.elasticbeanstalk.com/api/v1';
 
 export const createApiClient = (username, password) => {
   return axios.create({
